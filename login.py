88a30e4a6a165744b49221df067343166e7e8bb4
George Liu <zhihongliu491@gmail.com>
2023-05-31 17:30:22 +0800

    new version

--- login.py
+++ login.py
@@ -1,12 +1,22 @@
 import get_payload
 import requests
-import ip
+import socket
+
+
+def get_ip():
+    """get current ip address"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("10.50.255.11", 80))
+    current_ip = s.getsockname()[0]
+    s.close()
+
+    return current_ip
 
 
 def login():
     """get the request url """
     url = "http://10.50.255.11:801/eportal/"
-    current_ip = ip.get_ip()
+    current_ip = get_ip()
     params = {
         'c': 'ACSetting',
         'a': 'Login',
